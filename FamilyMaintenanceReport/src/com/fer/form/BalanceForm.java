7fbaaa89be60285f27a25e653c0067aa24e64d3f
jayasarikapathuri <jayasarikapathuri@DESKTOP-E3JD9SJ>
2019-12-28 15:32:22 +0530

    family

---
+++ FamilyMaintenanceReport/src/com/fer/form/BalanceForm.java
@@ -0,0 +1,64 @@
+package com.fer.form;
+
+import java.sql.Date;
+
+import org.apache.struts.action.ActionForm;
+
+
+public class BalanceForm extends ActionForm {
+	
+	int balanceId;
+	String actionType;
+	Float amount;
+	Date date;
+	String type;
+	int userId;
+	public int getBalanceId() {
+		return balanceId;
+	}
+	public void setBalanceId(int balanceId) {
+		this.balanceId = balanceId;
+	}
+	public String getActionType() {
+		return actionType;
+	}
+	public void setActionType(String actionType) {
+		this.actionType = actionType;
+	}
+	public Float getAmount() {
+		return amount;
+	}
+	public void setAmount(Float amount) {
+		this.amount = amount;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
